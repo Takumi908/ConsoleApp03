91ce262ee7965569b4771e398e02f1048c37075b
ShuheiYamada <31128@ojs.ac.jp>
2020-09-24 11:58:41 +0900

    ToUpper

--- ConsoleApp03/Program.cs
+++ ConsoleApp03/Program.cs
@@ -9,29 +9,24 @@ namespace ConsoleApp03
 {
     class Program
     {
-        //デリゲートの宣言'(int型の引数を受け取ってbool値を返すメソッド)
-        public delegate bool Judgment(int value);
-
         static void Main(string[] args)
         {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            //匿名メソッド
-            //int count = Count(numbers,delegate(int n)　 { return (n >= 5); });
-            int count = Count(numbers, n =>n.ToString().Contains('3'));  //5～8
 
-            Console.WriteLine(count);
-        }
-
-        public static int Count(int[] numbers, Judgment judge)
-        {
-            
-            int count = 0;
-            foreach(var n in numbers)
+            var list = new List<string>
             {
-                if (judge(n)==true)
-                    count++;
-            }
-            return count;
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Camberra","Hong Kong", 
+            };
+
+           // list.ForEach (s => Console.WriteLine(s)) ;
+
+            list.FindAll(s => s.ToUpper(s)).ForEach(s => Console.WriteLine(s));
+
+
+
+            /*
+           foreach (var s in name) 
+            Console.WriteLine(s);
+            */
         }
     }
 }
